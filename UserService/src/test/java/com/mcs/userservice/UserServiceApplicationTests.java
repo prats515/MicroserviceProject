80c900dc062685d11d5f164911a87ea8a0375588
prats515 <134759546+prats515@users.noreply.github.com>
2023-10-18 08:05:35 +0530

    Add files via upload

---
+++ UserService/src/test/java/com/mcs/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcs.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
